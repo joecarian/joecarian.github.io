9961cded25866af045d6a61c5c09ba90d6d2f4e0
joecarian <lgcairano@gmail.com>
2023-08-12 16:02:50 +0200

    Fixed contact form

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+const os = require("os");
+const API_EMAIL = os.environment.get("API_EMAIL");
+const SERVICE_ID = os.environment.get("SERVICE_ID");
+const TEMPLATE_ID = os.environment.get("TEMPLATE_ID");
